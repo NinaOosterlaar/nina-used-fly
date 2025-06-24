bd521f296b7e9b4cfe1ef5e8012b1328a0df5e98
Sebastiaan Beekman <sebas.beekman@gmail.com>
2025-06-24 22:28:08 +0200

    Index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nina Used Fly';
+export const SITE_DESCRIPTION = 'Making the world feel like home, one trip at a time.';
