07aadcc17678db775d90bd1b7595036cc55a98de
NinaOosterlaar <ninaoosterlaar@gmail.com>
2025-08-14 21:35:45 +0200

    fix random

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nina Used Fly';
-export const SITE_DESCRIPTION = 'Making the world feel like home, one trip at a time.';
+export const SITE_DESCRIPTION = 'Join the travels of Nina as she explores the world.';
